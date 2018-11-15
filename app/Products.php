7b31ab4253ce5ffb0f8dff82c0bc950fef0b1257
Augusto Miguel Zeponi <augustozeponi@hotmail.com>
2018-11-15 16:24:00 -0200

    #8 - Criando importação de arquivo CSV

--- app/Products.php
+++ app/Products.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
+	protected $fillable = ['name', 'description', 'image', 'price', 'status', 'category_id'];
+
     public function category() {
         return $this->belongsTo('App\Category', 'category_id');
     }
